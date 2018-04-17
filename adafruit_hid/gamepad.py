223aad98068aa47872755db99264cc1c4fcb04b1
Dan Halbert <halbert@halwitz.org>
2018-04-16 20:19:35 -0400

    WIP: gamepad + digitizer

--- adafruit_hid/gamepad.py
+++ adafruit_hid/gamepad.py
@@ -81,58 +81,32 @@ class Gamepad:
             time.sleep(1)
             self.reset_all()
 
-    def set_pressed_buttons(self, *buttons):
-        """Mark the given buttons as pressed.
-        Other buttons are not changed. Do not send a report yet."
-
-        Example::
-
-            g.clear_all_buttons()
-            g.set_buttons(3, 11)
-            # Send a report indicating buttons 3 and 11 are pressed.
-            g.send()
-        """
-
-        for button in buttons:
-            self._validate_button_number(button)
-            self._buttons_state |= 1 << button - 1
-
-
-    def set_released_buttons(self, *buttons):
-        """Mark the given buttons as released.
-        Other buttons are not changed. Do not send a report yet."
-        """
-
-        for button in buttons:
-            self._validate_button_number(button)
-            self._buttons_state &= ~(1 << button - 1)
-
     def press_buttons(self, *buttons):
         """Press and hold the given buttons. """
-
-        self.set_pressed_buttons(*buttons)
-        self.send()
+        for button in buttons:
+            self._buttons_state |= 1 << self._validate_button_number(button) - 1
+        self._send()
 
     def release_buttons(self, *buttons):
         """Release the given buttons. """
-
-        self.set_released_buttons(*buttons)
-        self.send()
+        for button in buttons:
+            self._buttons_state &= ~(1 << self._validate_button_number(button) - 1)
+        self._send()
 
     def release_all_buttons(self):
         """Release all the buttons."""
 
         self._buttons_state = 0
-        self.send()
+        self._send()
 
     def click_buttons(self, *buttons):
         """Press and release the given buttons."""
         self.press_buttons(*buttons)
         self.release_buttons(*buttons)
 
-    def set_joysticks(self, x=None, y=None, z=None, r_z=None):
-        """Set the given joystick values in the report to be sent.
-        Do not send a report yet.
+    def move_joysticks(self, x=None, y=None, z=None, r_z=None):
+        """Set and send the given joystick values.
+        The joysticks will remain set with the given values until changed
 
         One joystick provides ``x`` and ``y`` values,
         and the other provides ``z`` and ``r_z`` (z rotation).
@@ -143,58 +117,49 @@ class Gamepad:
         Examples::
 
             # Change x and y values only.
-            gp.set_joysticks(x=100, y=-50)
+            gp.move_joysticks(x=100, y=-50)
 
             # Reset all joystick values to center position.
-            gp.set_joysticks(0, 0, 0, 0)
+            gp.move_joysticks(0, 0, 0, 0)
         """
         if x is not None:
-            self._validate_joystick_value(x)
-            self._joy_x = x
+            self._joy_x = self._validate_joystick_value(x)
         if y is not None:
-            self._validate_joystick_value(y)
-            self._joy_y = y
+            self._joy_y = self._validate_joystick_value(y)
         if z is not None:
-            self._validate_joystick_value(z)
-            self._joy_z = z
+            self._joy_z = self._validate_joystick_value(z)
         if r_z is not None:
-            self._validate_joystick_value(r_z)
-            self._joy_r_z = r_z
-
-    def move_joysticks(self, x=None, y=None, z=None, r_z=None):
-        """Set and send the given joystick values.
-        The joysticks will remain set with the given values until changed."""
-        self.set_joysticks(x, y, z, r_z)
-        self.send()
+            self._joy_r_z = self._validate_joystick_value(r_z)
+        self._send()
 
     def reset_all(self):
         """Release all buttons and set joysticks to zero."""
         self._buttons_state = 0
         self.set_joysticks(0, 0, 0, 0)
-        self.send(always=True)
+        self._send(always=True)
 
-    def send(self, always=False):
+    def _send(self, always=False):
         """Send a report with all the existing settings.
         If ``always`` is ``False`` (the default), send only if there have been changes.
         """
-        self._report[0] = self._buttons_state & 0xff
-        self._report[1] = self._buttons_state >> 8
-        self._report[2] = self._joy_x
-        self._report[3] = self._joy_y
-        self._report[4] = self._joy_z
-        self._report[5] = self._joy_r_z
+        struct.pack_into('<HBBBB', self._report, 0,
+                         self._buttons_state,
+                         self._joy_x, self._joy_y,
+                         self._joy_z, self._joy_r_z)
 
         if always or self._last_report != self._report:
             self._hid_gamepad.send_report(self._report)
-            # Remember what we sent, without allocating a new report buffer.
+            # Remember what we sent, without allocating new storage.
             self._last_report[:] = self._report
 
     @staticmethod
     def _validate_button_number(button):
         if not 1 <= button <= 16:
             raise ValueError("Button number must in range 1 to 16")
+        return button
 
     @staticmethod
     def _validate_joystick_value(value):
         if not -127 <= value <= 127:
             raise ValueError("Joystick value must be in range -127 to 127")
+        return value
