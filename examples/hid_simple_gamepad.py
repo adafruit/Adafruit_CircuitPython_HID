2d056b02c77207958fb1c9d72e9cb2cc8068cb1e
foamyguy <foamyguy@gmail.com>
2021-08-20 17:46:42 -0500

    fix gamepad examples

--- examples/hid_simple_gamepad.py
+++ examples/hid_simple_gamepad.py
@@ -1,12 +1,17 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
+# You must add a gamepad HID device inside your boot.py file
+# in order to use this example.
+# See this Learn Guide for details:
+# https://learn.adafruit.com/customizing-usb-devices-in-circuitpython/hid-devices#custom-hid-devices-3096614-9
+
 import board
 import digitalio
 import analogio
 import usb_hid
 
-from gamepad import Gamepad
+from hid_gamepad import Gamepad
 
 gp = Gamepad(usb_hid.devices)
 
